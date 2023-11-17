61c6722aad5318916f6a5af1df67a24fa4ea61fd
RaphaelFL <52725627+RaphaelFL@users.noreply.github.com>
2023-11-17 12:28:25 -0300

    Initial commit

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,37 @@
+import Box from "@src/components/Box/Box";
+import { useTheme } from "@src/theme/ThemeProvider";
+import Background from "./patterns/Background/Background";
+import Feed from "./patterns/Feed/Feed";
+import Footer from "./patterns/Footer/Footer";
+import Menu from "./patterns/Menu/Menu";
+
+export default function HomeScreen() {
+  const theme = useTheme();
+
+  return (
+    <Box
+      tag="main"
+      styleSheet={{
+        backgroundColor: theme.colors.neutral.x000,
+        flex: 1,
+        alignItems: 'center',
+      }}
+    >
+      <Background />
+      <Menu />
+      <Feed>
+        <Feed.Header />
+      </Feed>
+      <Footer />
+      {/* 
+      <Feed>
+        <Feed.Header />
+        <Text tag="h2" variant="heading1">
+          Últimas Atualizações
+        </Text>
+        <Feed.Posts />
+      </Feed>
+       */}
+    </Box>
+  )
+}
